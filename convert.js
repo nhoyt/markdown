3d3b8523e43996ed951dab0fe645b8f8840c6b48
Nicholas Hoyt <nick@webui.com>
2020-08-18 11:37:38 -0500

    Update convert.js: process all .md files and output to ./html

--- convert.js
+++ convert.js
@@ -1,21 +1,40 @@
-const fs    = require('fs'),
-  showdown  = require('showdown'),
-  args      = process.argv.slice(2);
+const fs = require('fs'),
+      path = require('path'),
+      showdown = require('showdown'),
+      args = process.argv.slice(2);
 
-if (args.length === 0) {
-  return console.log('<<< Syntax: node convert [markdown-filename-without-extension] >>>');
+// Set the input file extension
+const inputExt = '.md';
+
+// Set the output directory (create if it doesn't exist)
+const outputDir = './html';
+
+if (!fs.existsSync(outputDir)){
+    fs.mkdirSync(outputDir);
 }
 
-const converter = new showdown.Converter( { 'noHeaderId': true } ),
-  markdown      = args[0] + '.md',
-  htmlFile      = args[0] + '.html';
+// Set the showdown options
+const options = {
+  'noHeaderId': true
+};
 
-fs.readFile(markdown, 'utf8', function (err, data) {
-  if (err) return console.log(err);
-  let html = converter.makeHtml(data);
+// Instantiate the showdown converter
+const converter = new showdown.Converter(options);
 
-  fs.writeFile(htmlFile, html, function (err) {
+// Define the converter function
+function md2html (markdown) {
+  let htmlFile = outputDir + '/' + path.basename(markdown, inputExt) + '.html';
+  fs.readFile(markdown, 'utf8', function (err, data) {
     if (err) return console.log(err);
-    console.log('Processed ' + markdown + ' -> wrote ' + htmlFile);
+    let html = converter.makeHtml(data);
+
+    fs.writeFile(htmlFile, html + '\n', function (err) {
+      if (err) return console.log(err);
+      console.log('Processed ' + markdown + ' -> wrote ' + htmlFile);
+    });
   });
-});
+}
+
+// Convert each markdown file in the current directory
+var files = fs.readdirSync('./').filter(filespec => filespec.endsWith(inputExt));
+files.forEach(file => { md2html(file) });
